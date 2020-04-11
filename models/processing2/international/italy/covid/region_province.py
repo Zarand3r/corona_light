7f235e4b49fe3037441b42edca554aa477ce45bb
Richard Bao <richardbao419@gmail.com>
2020-04-11 02:59:05 -0700

    update

--- models/processing2/international/italy/covid/region_province.py
+++ models/processing2/international/italy/covid/region_province.py
@@ -8,14 +8,15 @@ import git
 repo = git.Repo("./", search_parent_directories=True)
 homedir = repo.working_dir
 inputdir = f"{homedir}/data/international/italy/covid/"
+outputdir = f"{homedir}/models/data/international/italy/covid"
 
 # translate regional file
 dfr = pd.read_csv(inputdir + "dpc-covid19-ita-regioni.csv")
 dfr.columns = ["Date","Country", "Regional Code", "Region", "Latitude","Longitude","HospitalizedWithSymptoms","IntensiveCare","TotalHospitalized","HomeIsolation","TotalCurrentlyPositive","ChangeTotalPositive","NewCurrentlyPositive","DischargedHealed","Deaths","TotalCases","Tested","Note_IT","Note_ENG"]
-dfr.to_csv('dpc-covid19-ita-regioni.csv', index=False)
+dfr.to_csv(outputdir + 'dpc-covid19-ita-regioni.csv', index=False)
 
 # translate provincial file
 dfp = pd.read_csv(inputdir + "dpc-covid19-ita-province.csv")
 dfp.columns = ["Date","Country", "Regional Code", "Region", "Province Code","Province","ProvinceInitials","Latitude","Longitude","TotalCases","Note_IT","Note_ENG"]
-dfp.to_csv("dpc-covid19-ita-province.csv", index=False)
+dfp.to_csv(outputdir + "dpc-covid19-ita-province.csv", index=False)
 
