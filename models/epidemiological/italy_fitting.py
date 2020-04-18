5f544e608740b7455016a29283d9ec3034d2fd79
Richard Bao <richardbao419@gmail.com>
2020-04-18 05:49:11 -0700

    update

--- models/epidemiological/italy_fitting.py
+++ models/epidemiological/italy_fitting.py
@@ -29,11 +29,13 @@ sys.path.insert(1, f"{homedir}" + '/models/data_processing')
 import loader
 
 
-def process_data(data_covid, data_population):
+def process_data(data_covid, data_population, save=True):
 	covid = loader.load_data(data_covid)
 	loader.convert_dates(covid, "Date")
 	population = loader.load_data(data_population)
 	covid['Population'] = covid.apply(lambda row: loader.query(population, "Region", row.Region)['Population'], axis=1)
+	if save:
+		covid.to_csv("italy_training_data.csv")
 	return covid
 
 # return params, 1 standard deviation errors
@@ -71,29 +73,17 @@ def get_errors(res, p0):
 	perr = np.sqrt(np.diag(pcov))
 	return perr
 
-def get_errors2(res, p0):
-	# p0 = np.array(p0)
-	# ysize = len(res.fun)
-	# cost = 2 * res.cost  # res.cost is half sum of squares!
-	# popt = res.x
-
-	# _, s, VT = svd(res.jac, full_matrices=False)
-	# threshold = np.finfo(float).eps * max(res.jac.shape) * s[0]
-	# s = s[s > threshold]
-	# VT = VT[:s.size]
-	# pcov = np.dot(VT.T / s**2, VT)
-	# # s_sq = cost / (ysize - p0.size)
-	# # pcov = pcov * s_sq
-	# pcov = pcov/cost
-	# perr = np.sqrt(np.diag(pcov))
-	# print("##################################################################")
-	# print(perr)
-	# return perr
-	perr = np.sqrt(res.x)
-	print("##################################################################")
-	print(perr)
+# returns standard deviation of fitted parameters
+def get_param_errors(res, p0):
+	pfit = res.x
+	pcov = res.jac
+	pcov = np.dot(pcov.T, pcov)
+	pcov = np.linalg.pinv(pcov) #uses svd
+	pcov = np.diag(pcov)
+	rcov = np.cov(res.fun)/3750000
+	perr = pcov * rcov
+	perr = np.sqrt(perr)
 	return perr
-
 
 def mse_qd(A, B):
 	Ap = np.nan_to_num(A)
@@ -147,7 +137,40 @@ def pecaiqr(dat, t, params, N, max_t, offset):
 	dzdt = [dPdt, dEdt, dCdt, dAdt, dIdt, dQdt, dRdt, dDdt]
 	return dzdt
 
-def model_qd(params, data, tmax=-1):
+# def plot_pecaiqr():
+# 	n = 2000
+# 	# params = [0.3, 0.8, 0.1, 0.3, 0.6, 0.1, 0.01, 0.1, 0.8, 0.1, 0.1, 0.5, 0.1, 0.1, 0.1, 0.2, 0.5]
+# 	params = [0.1, 0.1, 0.1, 0.3, 0.5, 0.1, 0.1, 0.3, 0.5, 0.1, 0.1, 0.1, 0.001, 0.001, 0.001, 0.001, 0.001]
+# 	params[:] = [x/1  for x in params]
+# 	z0 = [75000, 25000, 100, 100, 100, 10, 10, 0]
+# 	t = np.linspace(0,500,n)
+# 	z = odeint(pecaiqr,z0,t, args = (params, ))
+
+# 	P = z[:,0]
+# 	E = z[:,1]
+# 	C = z[:,2]
+# 	A = z[:,3]
+# 	I = z[:,4]
+# 	Q = z[:,5]
+# 	R = z[:,6]
+# 	D = z[:,7]
+
+
+# 	# plot results
+# 	# plt.plot(t,P,'g--',label='P(t)')
+# 	# plt.plot(t,E,'b--',label='E(t)')
+# 	plt.plot(t,C,'b--',label='C(t)')
+# 	plt.plot(t,A,'y--',label='A(t)')
+# 	plt.plot(t,I,'r:',label='I(t)')
+# 	plt.plot(t,Q,'r:',label='Q(t)')
+# 	plt.plot(t,R,'k--',label='R(t)')
+# 	plt.plot(t,D,'k--',label='D(t)')
+# 	plt.ylabel('# people')
+# 	plt.xlabel('time')
+# 	plt.legend(loc='best')
+# 	plt.show()
+
+def model_qd(params, data, extrapolate=-1):
 	N = data['Population'].values[0] # total population
 	initial_conditions = N * np.array(params[-7:]) # the parameters are a fraction of the population so multiply by the population
 	P0 = initial_conditions[0]
@@ -162,8 +185,8 @@ def model_qd(params, data, tmax=-1):
 	yz_0 = np.array([P0, E0, C0, A0, I0, Q0, R0, D0])
 	
 	n = len(data)
-	if tmax > 0:
-		n = tmax
+	if extrapolate > 0:
+		n += extrapolate
 	
 	# Package parameters into a tuple
 	args = (params, N, n, offset)
@@ -178,8 +201,8 @@ def model_qd(params, data, tmax=-1):
 	return s
 
 
-def get_deaths(res, data, extrapolate=1):   
-	s = model_qd(res.x, data, len(data)*extrapolate)
+def get_deaths(res, data, extrapolate=14):   
+	s = model_qd(res.x, data, len(data)+extrapolate)
 	P = s[:,0]
 	E = s[:,1]
 	C = s[:,2]
@@ -188,13 +211,26 @@ def get_deaths(res, data, extrapolate=1):
 	Q = s[:,5]
 	R = s[:,6]
 	D = s[:,7]
-	tp = np.arange(0, len(data)*extrapolate)
+	tp = np.arange(0, len(data)+extrapolate)
 	deaths = list(zip(tp,D))
 	return deaths
 
+# returns uncertainty of the fit for all variables
+def get_fit_errors(res, p0_params, p0_initial_conditions, data, extrapolate=14):
+	errors = get_param_errors(res, list(p0_params) + list(p0_initial_conditions))
+	errors[len(p0_params):] = 0
+	uncertainty = []
+	samples = 100
+	for i in range(samples):
+		sample = np.random.normal(loc=res.x, scale=errors)
+		s = model_qd(sample, data, len(data)+extrapolate)
+		uncertainty.append(s)
+		
+	uncertainty = np.array(uncertainty)
+	return uncertainty
 
-def plot_qd(res, p0_params, p0_initial_conditions, data, extrapolate=1, boundary=None, plot_infectious=False):    
-	s = model_qd(res.x, data, len(data)*extrapolate)
+def plot_qd(res, p0_params, p0_initial_conditions, data, extrapolate=14, boundary=None, plot_infectious=False):   
+	s = model_qd(res.x, data, len(data)+extrapolate)
 	P = s[:,0]
 	E = s[:,1]
 	C = s[:,2]
@@ -205,7 +241,7 @@ def plot_qd(res, p0_params, p0_initial_conditions, data, extrapolate=1, boundary
 	D = s[:,7]
 
 	t = np.arange(0, len(data))
-	tp = np.arange(0, len(data)*extrapolate)
+	tp = np.arange(0, len(data)+extrapolate)
 
 	p = bokeh.plotting.figure(plot_width=600,
 							  plot_height=400,
@@ -232,26 +268,8 @@ def plot_qd(res, p0_params, p0_initial_conditions, data, extrapolate=1, boundary
 	p.legend.location = 'top_left'
 	bokeh.io.show(p)
 
-def plot_with_errors_sample(res, p0_params, p0_initial_conditions, data, extrapolate=1, boundary=None, plot_infectious=False):
-	print(res.x)
-	print(res.fun)
-	print(res.cost)
-	errors = get_errors2(res, list(p0_params) + list(p0_initial_conditions))
-	errors[len(p0_params):] = 0
-	all_s = []
-	samples = 100
-	for i in range(samples):
-		sample = np.random.normal(loc=res.x, scale=errors)
-		s = model_qd(sample, data, len(data)*extrapolate)
-		all_s.append(s)
-		
-	all_s = np.array(all_s)
-	s1 = np.percentile(all_s, 25, axis=0)
-	s2 = np.percentile(all_s, 75, axis=0)
-	print(s1[50])
-	print(s2[50])
-
-	s = model_qd(res.x, data, len(data)*extrapolate)
+def plot_with_errors_sample(res, p0_params, p0_initial_conditions, data, extrapolate=14, boundary=None, plot_infectious=False):
+	s = model_qd(res.x, data, len(data)+extrapolate)
 	P = s[:,0]
 	E = s[:,1]
 	C = s[:,2]
@@ -261,24 +279,19 @@ def plot_with_errors_sample(res, p0_params, p0_initial_conditions, data, extrapo
 	R = s[:,6]
 	D = s[:,7]
 
-	t = np.arange(0, len(data))
-	tp = np.arange(0, len(data)*extrapolate)
+	uncertainty = get_fit_errors(res, p0_params, p0_initial_conditions, data, extrapolate=14)
+	s1 = np.percentile(uncertainty, 25, axis=0)
+	s2 = np.percentile(uncertainty, 75, axis=0)
 
+	t = np.arange(0, len(data))
+	tp = np.arange(0, len(data)+extrapolate)
 	p = bokeh.plotting.figure(plot_width=600,
 							  plot_height=400,
-							 title = ' SEIIRD+Q Model Errors',
+							 title = ' PECAIQR Model Errors',
 							 x_axis_label = 't (days)',
 							 y_axis_label = '# people')
-
-	s1 = np.percentile(all_s, 25, axis=0)
-	s2 = np.percentile(all_s, 75, axis=0)
-
-	print(s1[-1])
-	print(s2[-1])
-
-	# if plot_infectious:
-		# p.varea(x=tp, y1=s1[:, 2], y2=s2[:, 2], color='red', fill_alpha=0.2)
-	# p.varea(x=tp, y1=s1[:, 3], y2=s2[:, 3], color='purple', fill_alpha=0.2)
+	if plot_infectious:
+		p.varea(x=tp, y1=s1[:, 4], y2=s2[:, 2], color='red', fill_alpha=0.2)
 	p.varea(x=tp, y1=s1[:, 7], y2=s2[:, 7], color='black', fill_alpha=0.2)
 	
 	if plot_infectious:
@@ -301,7 +314,7 @@ def plot_with_errors_sample(res, p0_params, p0_initial_conditions, data, extrapo
 	bokeh.io.show(p)
 
 
-def fit_leastsq_qd(params, data):
+def leastsq_qd(params, data, weight=False):
 	Ddata = (data['Deaths'].values)
 	Idata = (data['TotalCurrentlyPositive'].values)
 	s = model_qd(params, data)
@@ -314,36 +327,61 @@ def fit_leastsq_qd(params, data):
 	Q = s[:,5]
 	R = s[:,6]
 	D = s[:,7]
-	
+
 	error = D-Ddata
+	if weight:
+		# mu, sigma = 1, 0.2
+		# w = np.random.normal(mu, sigma, len(error))
+		# w = np.sort(w)
+		w = np.geomspace(0.5,1.5,len(data))
+		error = error*w
+
 	return error
 
 
-def main():
-	italy = process_data("/models/data/international/italy/covid/dpc-covid19-ita-regioni.csv", "/models/data/international/italy/demographics/region-populations.csv")
-	lombardia = loader.query(italy, "Region", "Lombardia")
-
-	# print(lombardia["totalcases"])
-
-	params = [0.1, 0.1, 0.1, 0.3, 0.5, 0.1, 0.1, 0.3, 0.5, 0.1, 0.1, 0.1, 0.001, 0.001, 0.001, 0.001, 0.001]
+def fit(data, weight=False, plot=False, extrapolate=14):
+	params = [9e-02, 1e-01, 7e-02, 3.e-01, 4.e-01, 1e-01, 1e-01, 3e-01, 4e-01, 7e-02, 2e-04, 8e-02, 7e-03, 2e-02, 2e-04, 2e-06, 4e-03]
 	params[:] = [x/1  for x in params]
 	param_ranges = [(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1)]
 	# initial_conditions = [0.74, 0.24, 0.005, 4e-6, 0.0009, 0.0005, 0.0002]
-	initial_conditions = [0.75, 0.25, 1e-5, 4e-6, 0, 0, 0]
+	initial_conditions = [7e-01, 2e-01, 4e-08, 7e-03, 1e-08, 3e-20, 7e-06]
 	initial_ranges = [(0,1), (0,1), (0,0.01), (0,0.01), (0,0.01), (0,0.01), (0,0.01)]
 	# guesses = params + initial_conditions
-	guesses = [9e-02, 1e-01, 7e-02, 3.e-01, 4.e-01, 1e-01, 1e-01, 3e-01, 4e-01, 7e-02, 2e-04, 8e-02, 7e-03, 2e-02, 2e-04, 2e-06, 4e-03, 7e-01, 2e-01, 4e-08, 7e-03, 1e-08, 3e-20, 7e-04]
-	ranges = param_ranges + initial_ranges
+	guesses = params+initial_conditions
+	ranges = param_ranges+initial_ranges
 
-
-	for boundary in [40]:
-		res = least_squares(fit_leastsq_qd, guesses, args=(lombardia[:boundary],), bounds=np.transpose(np.array(ranges)))
-		# plot_qd(res, params, initial_conditions, lombardia, extrapolate=2, boundary=boundary, plot_infectious=True)
-		# plot_with_errors_sample(res, params, initial_conditions, lombardia, extrapolate=1, boundary=boundary, plot_infectious=False)
-		predictions = get_deaths(res, lombardia, extrapolate=1)
+	for boundary in [len(data)]:
+		res = least_squares(leastsq_qd, guesses, args=(data[:boundary],weight), bounds=np.transpose(np.array(ranges)))
+		if plot:
+			plot_qd(res, params, initial_conditions, data, extrapolate=extrapolate, boundary=boundary, plot_infectious=True)
+			plot_with_errors_sample(res, params, initial_conditions, data, extrapolate=extrapolate, boundary=boundary, plot_infectious=False)
+		predictions = get_deaths(res, data, extrapolate=extrapolate)
+		errors = get_fit_errors(res, params, initial_conditions, data, extrapolate=extrapolate)
+		death_errors = errors[:,:,-1]
 		parameters = res.x 
-		print(predictions)
-		print(parameters)
+		cost = res.cost
+
+	# for boundary in [(0,30), (10,40), (20, 51)]:
+	# 	df = data[boundary[0]:boundary[1]]
+	# 	res = least_squares(leastsq_qd, guesses, args=(df,weight), bounds=np.transpose(np.array(ranges)))
+	# 	if plot:
+	# 		plot_qd(res, params, initial_conditions, df, extrapolate=14, boundary=boundary[1]-boundary[0], plot_infectious=True)
+	# 		# plot_with_errors_sample(res, params, initial_conditions, df, extrapolate=14, boundary=boundary[1]-boundary[0], plot_infectious=False)
+	# 	predictions = get_deaths(res, df, extrapolate=14)
+	# 	predictions = [(x+boundary[0],y) for (x,y) in predictions]
+	# 	parameters = res.x 
+	# 	cost = res.cost
+	# 	# store in list right now it only returns the last prediction and parameters
+	# 	print(parameters)
+	# 	print(predictions)
+	# 	print(cost)
+	return (predictions, death_errors, parameters, cost)
+
+def main(weight=True, plot=True):
+	#Get date range of April1 to June30 inclusive. Figure out how much to extrapolate
+	italy = process_data("/models/data/international/italy/covid/dpc-covid19-ita-regioni.csv", "/models/data/international/italy/demographics/region-populations.csv")
+	lombardia = loader.query(italy, "Region", "Lombardia")
+	fit(lombardia, weight=weight, plot=plot, extrapolate=14)
 
 if __name__ == '__main__':
 	main()
--- models/epidemiological/us_fitting.py
+++ models/epidemiological/us_fitting.py
@@ -1,7 +1,7 @@
-import itertools
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+import itertools
 
 import pandas as pd
 import numpy as np
@@ -29,44 +29,13 @@ sys.path.insert(1, f"{homedir}" + '/models/data_processing')
 import loader
 
 
-# def plot_pecaiqr():
-# 	n = 2000
-# 	# params = [0.3, 0.8, 0.1, 0.3, 0.6, 0.1, 0.01, 0.1, 0.8, 0.1, 0.1, 0.5, 0.1, 0.1, 0.1, 0.2, 0.5]
-# 	params = [0.1, 0.1, 0.1, 0.3, 0.5, 0.1, 0.1, 0.3, 0.5, 0.1, 0.1, 0.1, 0.001, 0.001, 0.001, 0.001, 0.001]
-# 	params[:] = [x/1  for x in params]
-# 	z0 = [75000, 25000, 100, 100, 100, 10, 10, 0]
-# 	t = np.linspace(0,500,n)
-# 	z = odeint(pecaiqr,z0,t, args = (params, ))
-
-# 	P = z[:,0]
-# 	E = z[:,1]
-# 	C = z[:,2]
-# 	A = z[:,3]
-# 	I = z[:,4]
-# 	Q = z[:,5]
-# 	R = z[:,6]
-# 	D = z[:,7]
-
-
-# 	# plot results
-# 	# plt.plot(t,P,'g--',label='P(t)')
-# 	# plt.plot(t,E,'b--',label='E(t)')
-# 	plt.plot(t,C,'b--',label='C(t)')
-# 	plt.plot(t,A,'y--',label='A(t)')
-# 	plt.plot(t,I,'r:',label='I(t)')
-# 	plt.plot(t,Q,'r:',label='Q(t)')
-# 	plt.plot(t,R,'k--',label='R(t)')
-# 	plt.plot(t,D,'k--',label='D(t)')
-# 	plt.ylabel('# people')
-# 	plt.xlabel('time')
-# 	plt.legend(loc='best')
-# 	plt.show()
-
-def process_data(data_covid, data_population):
+def process_data(data_covid, data_population, save=True):
 	covid = loader.load_data(data_covid)
-	loader.convert_dates(covid, "Date")
+	loader.convert_dates(covid, "date")
 	population = loader.load_data(data_population)
-	covid['Population'] = covid.apply(lambda row: loader.query(population, "Region", row.Region)['Population'], axis=1)
+	covid['Population'] = covid.apply(lambda row: loader.query(population, "FIPS", row.fips)['total_pop'], axis=1)
+	if save:
+		covid.to_csv("us_training_data.csv")
 	return covid
 
 # return params, 1 standard deviation errors
@@ -104,6 +73,17 @@ def get_errors(res, p0):
 	perr = np.sqrt(np.diag(pcov))
 	return perr
 
+# returns standard deviation of fitted parameters
+def get_param_errors(res, p0):
+	pfit = res.x
+	pcov = res.jac
+	pcov = np.dot(pcov.T, pcov)
+	pcov = np.linalg.pinv(pcov) #uses svd
+	pcov = np.diag(pcov)
+	rcov = np.cov(res.fun)/3750000
+	perr = pcov * rcov
+	perr = np.sqrt(perr)
+	return perr
 
 def mse_qd(A, B):
 	Ap = np.nan_to_num(A)
@@ -135,7 +115,6 @@ def pecaiqr(dat, t, params, N, max_t, offset):
 	d_i = params[15]
 	d_q = params[16]
 
-
 	P = dat[0]
 	E = dat[1]
 	C = dat[2]
@@ -157,7 +136,7 @@ def pecaiqr(dat, t, params, N, max_t, offset):
 	dzdt = [dPdt, dEdt, dCdt, dAdt, dIdt, dQdt, dRdt, dDdt]
 	return dzdt
 
-def model_qd(params, data, tmax=-1):
+def model_qd(params, data, extrapolate=-1):
 	N = data['Population'].values[0] # total population
 	initial_conditions = N * np.array(params[-7:]) # the parameters are a fraction of the population so multiply by the population
 	P0 = initial_conditions[0]
@@ -172,8 +151,8 @@ def model_qd(params, data, tmax=-1):
 	yz_0 = np.array([P0, E0, C0, A0, I0, Q0, R0, D0])
 	
 	n = len(data)
-	if tmax > 0:
-		n = tmax
+	if extrapolate > 0:
+		n += extrapolate
 	
 	# Package parameters into a tuple
 	args = (params, N, n, offset)
@@ -187,8 +166,37 @@ def model_qd(params, data, tmax=-1):
 
 	return s
 
-def plot_qd(res, p0_params, p0_initial_conditions, data, extrapolate=1, boundary=None, plot_infectious=False):    
-	s = model_qd(res.x, data, len(data)*extrapolate)
+
+def get_deaths(res, data, extrapolate=14):   
+	s = model_qd(res.x, data, len(data)+extrapolate)
+	P = s[:,0]
+	E = s[:,1]
+	C = s[:,2]
+	A = s[:,3]
+	I = s[:,4]
+	Q = s[:,5]
+	R = s[:,6]
+	D = s[:,7]
+	tp = np.arange(0, len(data)+extrapolate)
+	deaths = list(zip(tp,D))
+	return deaths
+
+# returns uncertainty of the fit for all variables
+def get_fit_errors(res, p0_params, p0_initial_conditions, data, extrapolate=14):
+	errors = get_param_errors(res, list(p0_params) + list(p0_initial_conditions))
+	errors[len(p0_params):] = 0
+	uncertainty = []
+	samples = 100
+	for i in range(samples):
+		sample = np.random.normal(loc=res.x, scale=errors)
+		s = model_qd(sample, data, len(data)+extrapolate)
+		uncertainty.append(s)
+		
+	uncertainty = np.array(uncertainty)
+	return uncertainty
+
+def plot_qd(res, p0_params, p0_initial_conditions, data, extrapolate=14, boundary=None, plot_infectious=False):   
+	s = model_qd(res.x, data, len(data)+extrapolate)
 	P = s[:,0]
 	E = s[:,1]
 	C = s[:,2]
@@ -199,7 +207,7 @@ def plot_qd(res, p0_params, p0_initial_conditions, data, extrapolate=1, boundary
 	D = s[:,7]
 
 	t = np.arange(0, len(data))
-	tp = np.arange(0, len(data)*extrapolate)
+	tp = np.arange(0, len(data)+extrapolate)
 
 	p = bokeh.plotting.figure(plot_width=600,
 							  plot_height=400,
@@ -226,20 +234,8 @@ def plot_qd(res, p0_params, p0_initial_conditions, data, extrapolate=1, boundary
 	p.legend.location = 'top_left'
 	bokeh.io.show(p)
 
-def plot_with_errors_sample(res, p0_params, p0_initial_conditions, data, extrapolate=1, boundary=None, plot_infectious=False):
-	errors = get_errors(res, list(p0_params) + list(p0_initial_conditions))
-	errors[len(p0_params):] = 0
-	
-	all_s = []
-	samples = 100
-	for i in range(samples):
-		sample = np.random.normal(loc=res.x, scale=errors)
-		s = model_qd(sample, data, len(data)*extrapolate)
-		all_s.append(s)
-		
-	all_s = np.array(all_s)
-
-	s = model_qd(res.x, data, len(data)*extrapolate)
+def plot_with_errors_sample(res, p0_params, p0_initial_conditions, data, extrapolate=14, boundary=None, plot_infectious=False):
+	s = model_qd(res.x, data, len(data)+extrapolate)
 	P = s[:,0]
 	E = s[:,1]
 	C = s[:,2]
@@ -249,27 +245,25 @@ def plot_with_errors_sample(res, p0_params, p0_initial_conditions, data, extrapo
 	R = s[:,6]
 	D = s[:,7]
 
-	t = np.arange(0, len(data))
-	tp = np.arange(0, len(data)*extrapolate)
+	uncertainty = get_fit_errors(res, p0_params, p0_initial_conditions, data, extrapolate=14)
+	s1 = np.percentile(uncertainty, 25, axis=0)
+	s2 = np.percentile(uncertainty, 75, axis=0)
 
+	t = np.arange(0, len(data))
+	tp = np.arange(0, len(data)+extrapolate)
 	p = bokeh.plotting.figure(plot_width=600,
 							  plot_height=400,
-							 title = ' SEIIRD+Q Model Errors',
+							 title = ' PECAIQR Model Errors',
 							 x_axis_label = 't (days)',
 							 y_axis_label = '# people')
-
-	s1 = np.percentile(all_s, 25, axis=0)
-	s2 = np.percentile(all_s, 75, axis=0)
-
 	if plot_infectious:
-		p.varea(x=tp, y1=s1[:, 2], y2=s2[:, 2], color='red', fill_alpha=0.2)
-	p.varea(x=tp, y1=s1[:, 3], y2=s2[:, 3], color='purple', fill_alpha=0.2)
-	p.varea(x=tp, y1=s1[:, 5], y2=s2[:, 5], color='black', fill_alpha=0.2)
+		p.varea(x=tp, y1=s1[:, 4], y2=s2[:, 2], color='red', fill_alpha=0.2)
+	p.varea(x=tp, y1=s1[:, 7], y2=s2[:, 7], color='black', fill_alpha=0.2)
 	
 	if plot_infectious:
-		p.line(tp, I, color = 'red', line_width = 1, legend = 'All infected')
+		p.line(tp, I, color = 'red', line_width = 1, legend = 'Currently Infected')
 	p.line(tp, D, color = 'black', line_width = 1, legend = 'Deceased')
-	p.line(tp, Q, color = 'yellow', line_width = 1, legend = 'Quarantined')
+	# p.line(tp, Q, color = 'yellow', line_width = 1, legend = 'Quarantined')
 	# p.line(tp, R, color = 'green', line_width = 1, legend = 'Recovered')
 
 	# death
@@ -286,8 +280,7 @@ def plot_with_errors_sample(res, p0_params, p0_initial_conditions, data, extrapo
 	bokeh.io.show(p)
 
 
-
-def fit_leastsq_qd(params, data):
+def leastsq_qd(params, data, weight=False):
 	Ddata = (data['Deaths'].values)
 	Idata = (data['TotalCurrentlyPositive'].values)
 	s = model_qd(params, data)
@@ -300,37 +293,67 @@ def fit_leastsq_qd(params, data):
 	Q = s[:,5]
 	R = s[:,6]
 	D = s[:,7]
-	
-	error = np.concatenate((D-Ddata, I-Idata))
+
+	error = D-Ddata
+	if weight:
+		# mu, sigma = 1, 0.2
+		# w = np.random.normal(mu, sigma, len(error))
+		# w = np.sort(w)
+		w = np.geomspace(0.5,1.5,len(data))
+		error = error*w
+
 	return error
 
 
-def main():
-	italy = process_data("/models/data/international/italy/covid/dpc-covid19-ita-regioni.csv", "/models/data/international/italy/demographics/region-populations.csv")
-	lombardia = loader.query(italy, "Region", "Lombardia")
-
-	# print(lombardia["totalcases"])
-
-	params = [0.1, 0.1, 0.1, 0.3, 0.5, 0.1, 0.1, 0.3, 0.5, 0.1, 0.1, 0.1, 0.001, 0.001, 0.001, 0.001, 0.001]
+def fit(data, weight=False, plot=False, extrapolate=14):
+	params = [9e-02, 1e-01, 7e-02, 3.e-01, 4.e-01, 1e-01, 1e-01, 3e-01, 4e-01, 7e-02, 2e-04, 8e-02, 7e-03, 2e-02, 2e-04, 2e-06, 4e-03]
 	params[:] = [x/1  for x in params]
 	param_ranges = [(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1)]
 	# initial_conditions = [0.74, 0.24, 0.005, 4e-6, 0.0009, 0.0005, 0.0002]
-	initial_conditions = [0.74, 0.24, 0.00001, 4e-6, 0.00001, 0.00001, 0.00001]
-	initial_ranges = [(0,1), (0,1), (0,0.3), (0,0.2), (0,0.5), (0,0.3), (0,1)]
-	guesses = params + initial_conditions
-	ranges = param_ranges + initial_ranges
+	initial_conditions = [7e-01, 2e-01, 4e-08, 7e-03, 1e-08, 3e-20, 7e-06]
+	initial_ranges = [(0,1), (0,1), (0,0.01), (0,0.01), (0,0.01), (0,0.01), (0,0.01)]
+	# guesses = params + initial_conditions
+	guesses = params+initial_conditions
+	ranges = param_ranges+initial_ranges
+
+	for boundary in [len(data)]:
+		res = least_squares(leastsq_qd, guesses, args=(data[:boundary],weight), bounds=np.transpose(np.array(ranges)))
+		if plot:
+			plot_qd(res, params, initial_conditions, data, extrapolate=extrapolate, boundary=boundary, plot_infectious=True)
+			plot_with_errors_sample(res, params, initial_conditions, data, extrapolate=extrapolate, boundary=boundary, plot_infectious=False)
+		predictions = get_deaths(res, data, extrapolate=extrapolate)
+		errors = get_fit_errors(res, params, initial_conditions, data, extrapolate=extrapolate)
+		death_errors = errors[:,:,-1]
+		parameters = res.x 
+		cost = res.cost
+
+	return (predictions, death_errors, parameters, cost)
 
 
-	for boundary in [30, 40]:
-		res = least_squares(fit_leastsq_qd, guesses, args=(lombardia[:boundary],), bounds=np.transpose(np.array(ranges)))
-		plot_qd(res, params, initial_conditions, lombardia, extrapolate=2, boundary=boundary, plot_infectious=True)
-		# plot_with_errors_sample(res, params, initial_conditions, lombardia, extrapolate=2, boundary=boundary, plot_infectious=True)
 
 
+###########################################################
+
+def main(weight=True, plot=False):
+	#Get date range of April1 to June30 inclusive. Figure out how much to extrapolate
+	us = process_data("/data/us/covid/nyt_us_counties.csv", "/data/us/demographics/county_populations.csv")
+
+	#iterate through all fips in the column
+	# county = loader.query(us, "fips", fips)
+	# _, death_errors, _ , _ = fit(county, weight=weight, plot=plot, extrapolate=14)
+
+	# submission=[]
+
+	# for percentile in [10, 20, 30, 40, 50, 60, 70, 80, 90]:
+	# 	forecast = list(np.percentile(death_errors, percentile, axis=0))
+	# 	submission.append(forecast)
+
+	# submission = np.transpose(submission)
+	# print(submission)
+	# #output 4 tuple of (date, fips, time, )
 
 if __name__ == '__main__':
 	main()
-	# plot_pecaiqr()
 
 
 
