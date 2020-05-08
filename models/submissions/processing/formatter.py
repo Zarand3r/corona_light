efebf7c1de9d0abb886344ab71ccbc298f195ca0
Richard Bao <richardbao419@gmail.com>
2020-05-08 01:30:37 -0700

    Revert "update"
    This reverts commit 6bf6c57914b8de5b03e066e6628840ed2badf9f9.

---
+++ models/submissions/processing/formatter.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import islice
+import datetime
+from datetime import timedelta
+import csv
+
+
+import git
+import sys
+repo = git.Repo("./", search_parent_directories=True)
+homedir = repo.working_dir
+# FIXES NANs
+
+def fix_nans(submission, save=False):
+	submission.fillna(method='ffill', inplace=True)
+	# Do this stuff in the code that generates the error bounds instead of here
+	num = submission._get_numeric_data()
+	num[num < 0.1] = 0.00
+	if save:
+		submission.to_csv("modified_submission.csv", index=False)
+
+def reformat(file1, file2, save=True):
+	submission = pd.read_csv(file1, index_col=False)
+	modified_submission = pd.read_csv(file2, index_col=False)
+	fix_nans(submission)
+	# convert to floats
+	submission[["10", "20", "30", "40", "50", "60", "70", "80", "90"]] = submission[["10", "20", "30", "40", "50", "60", "70", "80", "90"]].apply(pd.to_numeric)
+
+	final_submission = []
+
+	forecast_dict = {}
+	for index, row in submission.iterrows():
+		current_id = row["id"]
+		forecast = row.values
+		# Do this stuff in the code that generates the error bounds instead of here
+		limit = 50000.00
+		if forecast[-1] > limit:
+			for i in range(1,6):
+				if forecast[i] > limit:
+					forecast[i] = limit
+			thresh = forecast[5]
+			row[6] = 1.05*thresh
+			row[7] = 1.1*thresh
+			row[8] = 1.15*thresh
+			row[9] = 1.2*thresh
+		forecast_dict[current_id] = row.values
+
+	end = len(modified_submission)
+	# for index, row in islice(modified_submission.iterrows(), start, end):
+	for index, row in modified_submission.iterrows():
+		print(f"{index} / {end}")
+		current_id = row["id"]
+		replacement = forecast_dict.pop(current_id, [current_id, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]) 
+		final_submission.append(replacement)
+
+	if save:
+		header = ["id", "10", "20", "30", "40", "50", "60", "70", "80", "90"]
+		with open('../model1/formatted_prediction.csv', 'w') as submission_file:
+			writer = csv.writer(submission_file, delimiter=',')
+			writer.writerow(header)
+			writer.writerows(final_submission)
+
+	
+
+if __name__ == '__main__':
+
+	reformat("../predictions.csv", f"{homedir}/sample_submission.csv")
