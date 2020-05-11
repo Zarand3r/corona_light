9e83401daa2e2d51259770da2dc1bb032086173f
Richard Bao <richardbao419@gmail.com>
2020-05-10 18:38:30 -0700

    update

--- models/submissions/evaluator.py
+++ models/submissions/evaluator.py
@@ -10,7 +10,7 @@ homedir = repo.working_dir
 sys.path.insert(1, f"{homedir}" + '/models/data_processing')
 import loader
 
-csv_to_score = 'checkpoint2/submission2_1_1.csv'
+csv_to_score = 'checkpoint2/submission2_1_2.csv'
 print(f"scoring {csv_to_score}")
 # print("scoring submission2_0_1.csv")
 # csv_to_score = 'checkpoint1/submission2_0_1.csv'
