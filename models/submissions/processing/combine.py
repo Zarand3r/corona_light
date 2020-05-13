872a47551edac62e1887e13d866c3e7d1c447c35
Richard Bao <richardbao419@gmail.com>
2020-05-13 09:25:31 -0700

    update

--- models/submissions/processing/combine.py
+++ models/submissions/processing/combine.py
@@ -152,7 +152,7 @@ def evaluator(submission, start_date):
 
 if __name__ == '__main__':
     start_date = '2020-05-09'
-    submissions = [f"{homedir}"+ '/sample_submission.csv', '../model1/version3_0/old/submission3_0_0.csv', '../model1/version3_0/old/submission3_0_1.csv', '../model1/version3_0/old/submission3_0_2.csv']
+    submissions = [f"{homedir}"+ '/sample_submission.csv', '../model1/version3_0/old/submission3_0_0.csv', '../model1/version3_0/old/submission3_0_1.csv', '../model1/version3_0/submission3_0_2.csv']
     scores = []
     
     for submission in submissions:
