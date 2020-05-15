7dda001697ca92a0b2a0bee9d0c5ca39d5540a05
Richard Bao <richardbao419@gmail.com>
2020-05-14 19:12:07 -0700

    update

--- models/epidemiological/production/fit_counties3_0.py
+++ models/epidemiological/production/fit_counties3_0.py
@@ -1057,7 +1057,7 @@ def fit_single_county(input_dict):
 	county_data = loader.query(us, "fips", county)
 	county_data['daily_deaths'] = loader.query(us_daily, "fips", county)["deaths"]
 	county_data['avg_deaths'] = county_data.iloc[:,6].rolling(window=3).mean()
-	county_data = county_data[2:-3]
+	county_data = county_data[2:]
 
 	firstnonzero = next((index for index,value in enumerate(county_data[death_metric].values) if value != 0), None)
 	if firstnonzero is not None:
